843185db59e38ed65d4cac77fa4f22f5ab46b846
rakshita <raksh@gmail.com>
2025-11-12 22:17:05 +0530

    fifth version

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 a = input("enter a number")
 b = input("enter a number")
-if a % 2 = 0 & b % 2 = 0:
-    print("it is an even number")
+if a % 2 == 0 and b % 2 == 0:
+    print("Both numbers are even.")
+elif a % 2 != 0 and b % 2 != 0:
+    print("Both numbers are odd.")
+elif a % 2 == 0 and b % 2 != 0:
+    print("First number is even and second number is odd.")
 else:
-    print("its an odd number")
+    print("First number is odd and second number is even.")
